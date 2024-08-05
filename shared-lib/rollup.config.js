7e2957e21a68d4bcce5818679b1ac90672aa952f
DexterHill0 <49513414+DexterHill0@users.noreply.github.com>
2024-08-05 13:49:46 +0100

    use rollup to transpile shared-lib

---
+++ shared-lib/rollup.config.js
@@ -0,0 +1,31 @@
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
+import { globSync } from "glob";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+export default [
+    {
+        input: Object.fromEntries(
+            globSync("src/**/*.ts").map(file => [
+                path.relative(
+                    "src",
+                    file.slice(0, file.length - path.extname(file).length)
+                ),
+
+                fileURLToPath(new URL(file, import.meta.url)),
+            ])
+        ),
+        cache: false,
+        output: {
+            dir: "dist",
+            format: "es",
+            preserveModules: true,
+            preserveModulesRoot: "src",
+        },
+        plugins: [
+            typescript({ tsconfig: "./tsconfig.json" }),
+            json({ compact: true, preferConst: true }),
+        ],
+    },
+];

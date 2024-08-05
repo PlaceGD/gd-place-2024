742caa9dbd5fe4ffead640c71e0f9abeeae07888
DexterHill0 <49513414+DexterHill0@users.noreply.github.com>
2024-08-05 14:13:41 +0100

    use rollup in cloud functions, add terser to minify js

--- functions/rollup.config.js
+++ functions/rollup.config.js
@@ -1,22 +1,10 @@
 import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
-import { globSync } from "glob";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import terser from "@rollup/plugin-terser";
 
 export default [
     {
-        input: Object.fromEntries(
-            globSync("src/**/*.ts").map(file => [
-                path.relative(
-                    "src",
-                    file.slice(0, file.length - path.extname(file).length)
-                ),
-
-                fileURLToPath(new URL(file, import.meta.url)),
-            ])
-        ),
+        input: "./src/index.ts",
         cache: false,
         output: {
             dir: "dist",

1e8e24af31e0503e43c3545e907d54fe9faf9444
August Ekenes Asp <augusea@stud.ntnu.no>
2024-08-13 17:19:57 +0200

    report button animation and settings refactor

--- src/moderator/ReportedUserList.svelte
+++ src/moderator/ReportedUserList.svelte
@@ -108,94 +108,99 @@
     };
 </script>
 
-{#if $openMenu == ExclusiveMenus.Moderator}
-    <div
-        class="z-50 flex flex-col py-2 gap-2 mr-6 text-lg text-white rounded-lg sm:mr-4 w-96 xs:w-80 menu-panel flex-center pointer-events-all max-h-[75%]"
-    >
-        <h1
-            class="text-2xl text-center sm:text-xl xs:text-lg font-pusab text-stroke"
-        >
-            Reported Users:
-        </h1>
-        {#if reportedUsers != null && Object.keys(reportedUsers).length == 0}
-            <p class="text-lg sm:text-sm xs:text-sm">
-                No users have been reported!
-            </p>
-        {:else if reportedUsers != null}
-            <FadedScroll>
-                <ul
-                    class="flex flex-col w-full gap-2 px-6 overflow-y-auto rounded-lg xs:px-4"
-                >
-                    {#each Object.values(reportedUsers) as user, idx}
-                        <li
-                            class="relative flex-col w-full gap-2 p-2 rounded-lg flex-center even:bg-white/5 odd:bg-black/15"
-                        >
-                            <div
-                                class="grid items-center justify-center w-full grid-cols-3 grid-rows-1"
-                            >
-                                <p class="col-start-2">
-                                    <span class="text-base xs:text-sm"
-                                        >{user.username}</span
-                                    >
-                                    <span class="text-sm xs:text-xs"
-                                        >(x{user.count})</span
-                                    >
-                                </p>
-                                <div
-                                    class="w-8 h-8 col-start-3 justify-self-end"
-                                >
-                                    <Button
-                                        title="View average report location"
-                                        class="w-full h-full"
-                                        type="plain"
-                                        on:click={() => {
-                                            const data = reportedUsers[idx];
 
-                                            moveCamera(
-                                                state,
-                                                data.sumX / data.count,
-                                                data.sumY / data.count
-                                            );
-                                        }}
-                                    >
-                                        <Back class="rotate-180 stroke-[1.5]"
-                                        ></Back>
-                                    </Button>
-                                </div>
-                            </div>
-                            <div class="flex w-full h-10 gap-2 xs:h-9">
-                                <Button
-                                    type="decline"
-                                    class="w-full h-full"
-                                    on:click={() => {
-                                        userOp("ignore", user.uid, idx);
-                                    }}
-                                    disabled={currentIdx == idx}
+<div
+    class="z-50 flex flex-col py-2 gap-2 mr-6 text-lg text-white rounded-lg sm:mr-4 w-96 xs:w-80 menu-panel flex-center pointer-events-all max-h-[75%]"
+    style={`
+            height: ${$openMenu != ExclusiveMenus.Moderator ? "0" : "50vh"};
+            opacity: ${$openMenu != ExclusiveMenus.Moderator ? "0" : "1"};
+            transition: height 0.2s ease, opacity 0.2s ease-out;
+        `}
+>
+    <h1
+        class="text-2xl text-center sm:text-xl xs:text-lg font-pusab text-stroke"
+    >
+        Reported Users:
+    </h1>
+    {#if reportedUsers != null && Object.keys(reportedUsers).length == 0}
+        <p class="text-lg sm:text-sm xs:text-sm">
+            No users have been reported!
+        </p>
+    {:else if reportedUsers != null}
+        <FadedScroll>
+            <ul
+                class="flex flex-col w-full gap-2 px-6 overflow-y-auto rounded-lg xs:px-4"
+            >
+                {#each Object.values(reportedUsers) as user, idx}
+                    <li
+                        class="relative flex-col w-full gap-2 p-2 rounded-lg flex-center even:bg-white/5 odd:bg-black/15"
+                    >
+                        <div
+                            class="grid items-center justify-center w-full grid-cols-3 grid-rows-1"
+                        >
+                            <p class="col-start-2">
+                                <span class="text-base xs:text-sm"
+                                    >{user.username}</span
                                 >
-                                    <span class="text-sm">Ignore</span>
-                                </Button>
-                                <Button
-                                    type="accept"
-                                    class="w-full h-full"
-                                    on:click={() => {
-                                        userOp("ban", user.uid, idx);
-                                    }}
-                                    disabled={currentIdx == idx}
+                                <span class="text-sm xs:text-xs"
+                                    >(x{user.count})</span
                                 >
-                                    <span class="text-sm">Ban</span>
+                            </p>
+                            <div
+                                class="w-8 h-8 col-start-3 justify-self-end"
+                            >
+                                <Button
+                                    title="View average report location"
+                                    class="w-full h-full"
+                                    type="plain"
+                                    on:click={() => {
+                                        const data = reportedUsers[idx];
+
+                                        moveCamera(
+                                            state,
+                                            data.sumX / data.count,
+                                            data.sumY / data.count
+                                        );
+                                    }}
+                                >
+                                    <Back class="rotate-180 stroke-[1.5]"
+                                    ></Back>
                                 </Button>
                             </div>
-                            {#if currentIdx == idx}
-                                <Loading class="rounded-lg" />
-                            {/if}
-                        </li>
-                    {/each}
-                </ul>
-            </FadedScroll>
-            <!-- {:else}
-            <div class="relative w-12 h-12">
-                <Loading darken={false} />
-            </div> -->
-        {/if}
-    </div>
-{/if}
+                        </div>
+                        <div class="flex w-full h-10 gap-2 xs:h-9">
+                            <Button
+                                type="decline"
+                                class="w-full h-full"
+                                on:click={() => {
+                                    userOp("ignore", user.uid, idx);
+                                }}
+                                disabled={currentIdx == idx}
+                            >
+                                <span class="text-sm">Ignore</span>
+                            </Button>
+                            <Button
+                                type="accept"
+                                class="w-full h-full"
+                                on:click={() => {
+                                    userOp("ban", user.uid, idx);
+                                }}
+                                disabled={currentIdx == idx}
+                            >
+                                <span class="text-sm">Ban</span>
+                            </Button>
+                        </div>
+                        {#if currentIdx == idx}
+                            <Loading class="rounded-lg" />
+                        {/if}
+                    </li>
+                {/each}
+            </ul>
+        </FadedScroll>
+        <!-- {:else}
+        <div class="relative w-12 h-12">
+            <Loading darken={false} />
+        </div> -->
+    {/if}
+</div>
+

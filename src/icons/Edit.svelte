5689a57a59f8c0e7c43ef1c87de952bf33f9a426
DexterHill0 <49513414+DexterHill0@users.noreply.github.com>
2024-09-11 13:19:02 +0100

    add icons

---
+++ src/icons/Edit.svelte
@@ -0,0 +1,69 @@
+<svg
+    viewBox="0 0 36 36"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="currentColor"
+    {...$$props}
+>
+    <g clip-path="url(#clip0_316_301)">
+        <path
+            d="M18 9C18 9.79565 18.3161 10.5587 18.8787 11.1213C19.4413 11.6839 20.2044 12 21 12C21.7956 12 22.5587 11.6839 23.1213 11.1213C23.6839 10.5587 24 9.79565 24 9C24 8.20435 23.6839 7.44129 23.1213 6.87868C22.5587 6.31607 21.7956 6 21 6C20.2044 6 19.4413 6.31607 18.8787 6.87868C18.3161 7.44129 18 8.20435 18 9Z"
+            stroke-width="2.25"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M6 9H18"
+            stroke-width="2.25"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M24 9H30"
+            stroke-width="2.25"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M9 18C9 18.7956 9.31607 19.5587 9.87868 20.1213C10.4413 20.6839 11.2044 21 12 21C12.7956 21 13.5587 20.6839 14.1213 20.1213C14.6839 19.5587 15 18.7956 15 18C15 17.2044 14.6839 16.4413 14.1213 15.8787C13.5587 15.3161 12.7956 15 12 15C11.2044 15 10.4413 15.3161 9.87868 15.8787C9.31607 16.4413 9 17.2044 9 18Z"
+            stroke-width="2.25"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M6 18H9"
+            stroke-width="2.25"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M15 18H30"
+            stroke-width="2.25"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M22.5 27C22.5 27.7956 22.8161 28.5587 23.3787 29.1213C23.9413 29.6839 24.7044 30 25.5 30C26.2956 30 27.0587 29.6839 27.6213 29.1213C28.1839 28.5587 28.5 27.7956 28.5 27C28.5 26.2044 28.1839 25.4413 27.6213 24.8787C27.0587 24.3161 26.2956 24 25.5 24C24.7044 24 23.9413 24.3161 23.3787 24.8787C22.8161 25.4413 22.5 26.2044 22.5 27Z"
+            stroke-width="2.25"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M6 27H22.5"
+            stroke-width="2.25"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M28.5 27H30"
+            stroke-width="2.25"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+    </g>
+    <defs>
+        <clipPath id="clip0_316_301">
+            <rect width="36" height="36" fill="white" />
+        </clipPath>
+    </defs>
+</svg>
